c13c2b3cea018a3f3c349c90cca5ddd689cc9ddd
franky-lim24 <56810823+Franky-Lim24@users.noreply.github.com>
2023-08-25 20:11:04 +0800

    add shadcn and configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './components/**/*.{html,js,jsx}',
+    './pages/**/*.{html,js,jsx}',
+    './index.html',
+    './public/index.html',
+    './src/**/*.{html,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

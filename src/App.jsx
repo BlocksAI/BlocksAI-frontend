b56efa2e1bb986212c114fd5bf46a3105d050a5a
Franky <franky.2021@scis.smu.edu.sg>
2023-08-26 14:56:31 +0800

    auth page animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/pages/Auth';
+import Marketplace from './components/pages/Marketplace';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './components/pages/Error';
 
 function App() {
   const [name, setName] = useState();
@@ -12,7 +15,24 @@ function App() {
     }
   }, []);
 
-  return <>{name ? <Marketplace /> : <Auth />}</>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: '/auth',
+          element: <Auth />,
+        },
+        {
+          path: '/market',
+          element: <Marketplace />,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;

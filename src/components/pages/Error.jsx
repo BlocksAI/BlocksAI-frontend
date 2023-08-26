c5c662ee8c8a7d8ce62e4eebc962475bf952098a
Franky <franky.2021@scis.smu.edu.sg>
2023-08-26 15:15:12 +0800

    Update Error.jsx

--- src/components/pages/Error.jsx
+++ src/components/pages/Error.jsx
@@ -5,7 +5,10 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
+    <div
+      id="error-page"
+      className="w-screen h-screen flex items-center justify-center flex-col gap-4"
+    >
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>

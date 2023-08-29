e08ad163ed5a859ba473c705db52a183bccdc103
franky-lim24 <56810823+Franky-Lim24@users.noreply.github.com>
2023-08-29 15:00:13 +0800

    Update gateway.js

--- src/utils/gateway.js
+++ src/utils/gateway.js
@@ -1,16 +1,23 @@
 import instance from './axiosInstance';
 
 const blockPicker = async (prompt) => {
-  const response = await instance.post('/blocks-gateway/gateway', {
+  var response = await instance.post('/blocks-gateway/gateway', {
     user_prompt: prompt,
   });
+  while (
+    response.data.chosen_block !== ' PersonalEducationPlanner' &&
+    response.data.chosen_block !== ' CarbonFootprintTracker'
+  ) {
+    response = await instance.post('/blocks-gateway/gateway', {
+      user_prompt: prompt,
+    });
+  }
   return response.data;
 };
 
 const chosenBlock = async (blockName, userPrompt) => {
-  console.log(blockName.trim());
   const response = await instance.post('/blocks-gateway/chosen-block', {
-    block_name: 'PersonalEducationPlanner',
+    block_name: blockName.trim(),
     user_prompt: userPrompt,
   });
   return response.data;
